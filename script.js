45520d5386222ea4021cfe7dc5910df70df4e8fb
Preetam Hegde <75658484+Preetam-ptwo@users.noreply.github.com>
2022-10-23 23:34:37 +0530

    2

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-$(function() {
-    $("button").on({
-      mouseover: function() {
-        $(this).css({
-          left: (Math.random() * 200) + "px",
-          top: (Math.random() * 200) + "px",
-        });
-      }
-    });
-});
+$(document).ready(function() {
 
-function showAlert() {
-  alert ("YAA Thanks Love!");
-}
+    // Transition effect for navbar and back-to-top icon
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 550) { 
+          $('.navbar').addClass('solid');
+          $('.back-to-top').addClass('visible'); 
+      } else {
+          $('.navbar').removeClass('solid');
+          $('.back-to-top').removeClass('visible');  
+      }
+
+    });
+
+
+    // Scrolling effect for Arrow icons
+    $("#scrollIcon").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#about"), 1000);
+    });
+    $("#nav-about").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#about"), 1000);
+    });
+    $("#nav-portfolio").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#portfolio"), 1000);
+    });
+    $("#nav-contact").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#contact"), 1000);
+    });
+    $(".navbar-brand").click(function(e) {
+        e.preventDefault();
+        $.scrollTo(0, 1000);
+    });
+      
+  });
